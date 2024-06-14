6fb07b89bfd550f937e9b8f91e1c590cd548a9ef
plusdojo2024-takahashi <takahashi-ryo-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 16:36:40 +0900

    jsp

--- src/servlet/HouseRegistServlet.java
+++ src/servlet/HouseRegistServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HouseSelectServlet
+ * Servlet implementation class HouseRegistServlet
  */
-@WebServlet("/HouseSelectServlet")
-public class HouseSelectServlet extends HttpServlet {
+@WebServlet("/HouseRegistServlet")
+public class HouseRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HouseSelectServlet() {
+    public HouseRegistServlet() {
         super();
         // TODO Auto-generated constructor stub
     }

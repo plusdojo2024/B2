3e948efc6209e73f95974df2e68dc63a47798126
asano-yukii <asano-yuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 14:10:19 +0900

    浅野更新2

--- src/model/Approvals.java
+++ src/model/Approvals.java
@@ -1,5 +1,55 @@
 package model;
 
-public class Approvals {
+import java.io.Serializable;
 
+public class Approvals implements Serializable {
+
+	private int ID; //ID
+	private String settlements_id; //精算ID
+	private String users_id; //ユーザーID
+	private String confirm; //支払チェック済か
+
+	public Approvals() {
+
+	}
+
+	public Approvals(int ID, String settlements_id, String users_id, String confirm) {
+		super();
+		this.ID = ID;
+		this.settlements_id = settlements_id;
+		this.users_id = users_id;
+		this.confirm = confirm;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public String getSettlements_id() {
+		return settlements_id;
+	}
+
+	public void setSettlements_id(String settlements_id) {
+		this.settlements_id = settlements_id;
+	}
+
+	public String getUsers_id() {
+		return users_id;
+	}
+
+	public void setUsers_id(String users_id) {
+		this.users_id = users_id;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
 }

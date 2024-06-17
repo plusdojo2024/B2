c059a78295b9a91ff18caf58709a210260bcf971
plusdojo-ito-sky <ito-sora-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 14:11:41 +0900

    a

--- src/model/Expenses.java
+++ src/model/Expenses.java
@@ -1,5 +1,65 @@
 package model;
 
-public class Expenses {
+import java.io.Serializable;
 
+public class Expenses implements Serializable{
+	private int ID; //ID
+	private String expense_type; //支出の種類
+	private int amount; //金額
+	private String description; //詳細
+	private int expense_date; //支払日
+
+	public Expenses() {
+
+	}
+
+
+	public Expenses(int ID, String expense_type, int amount, String description, int expense_date) {
+		super();
+		this.ID = ID;
+		this.expense_type = expense_type;
+		this.amount = amount;
+		this.description = description;
+		this.expense_date = expense_date;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public String getExpense_type() {
+		return expense_type;
+	}
+
+	public void setExpense_type(String expense_type) {
+		this.expense_type = expense_type;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getExpense_date() {
+		return expense_date;
+	}
+
+	public void setExpense_date(int expense_date) {
+		this.expense_date = expense_date;
+	}
 }
--- src/model/Schedules.java
+++ src/model/Schedules.java
@@ -1,5 +1,63 @@
 package model;
 
-public class Schedules {
+import java.io.Serializable;
+
+public class Schedules implements Serializable {
+	private int ID; //ID
+	private int users_id; //ユーザーID
+	private String title; //タイトル
+	private String text; //詳細
+	private int date; //日時
+
+	public Schedules(int ID, int users_id, String title, String text, int date) {
+		super();
+		this.ID = ID;
+		this.users_id = users_id;
+		this.title = title;
+		this.text = text;
+		this.date = date;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public int getUsers_id() {
+		return users_id;
+	}
+
+	public void setUsers_id(int users_id) {
+		this.users_id = users_id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+
 
 }

3d0585e09038698f8a85cb50ea60a094a3bedab4
plusdojo-ito-sky <ito-sora-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 16:48:03 +0900

    a

--- src/servlet/ItemRegistServlet.java
+++ src/servlet/ItemRegistServlet.java
@@ -9,20 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ItemDAO;
+import model.Items;
+
 /**
  * Servlet implementation class ItemServlet
  */
 @WebServlet("/ItemServlet")
 public class ItemRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemRegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -35,7 +30,28 @@ public class ItemRegistServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		int ID = Integer.parseInt(request.getParameter("ID"));
+		String item_name = request.getParameter("item_name");
+		int task_details_id = Integer.parseInt(request.getParameter("task_details_id"));
+		int status = Integer.parseInt(request.getParameter("status"));
+
+		//在庫品の登録処理
+		ItemDAO itemDao = new ItemDAO();
+
+		if(itemDao.insert(new Items(0,item_name, 0, task_details_id , 0))) {
+			System.out.println("登録成功！");
+		}
+		else {
+			System.out.println("登録失敗！");
+		}
 
 
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/house_login.jsp");
+		dispatcher.forward(request, response);
+	}
 
 }

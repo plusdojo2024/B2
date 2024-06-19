4593b290e5646afd92a2e5920f288efc2b43aef1
asano-yukii <asano-yuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 13:01:00 +0900

    浅野

--- src/servlet/ItemListServlet.java
+++ src/servlet/ItemListServlet.java
@@ -2,35 +2,35 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Items;
+import dao.ItemDAO;
 
 /**
  * Servlet implementation class ItemListServlet
- */
+
+@@ -30,21 +27,16 @@ public class ItemListServlet extends HttpServlet {
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 @WebServlet("/ItemListServlet")
 public class ItemListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
+	//表示処理を行う
+		ItemDAO itemDAO= new ItemDAO();
+		if (itemDAO.selectAll(new Items(0,null,0,0,houses_id))) {	// 表示成功
+			System.out.println("表示成功");
+		}
+		else {												// 表示失敗
+			System.out.println("表示失敗");
+		}
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -38,5 +38,5 @@ public class ItemListServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
+

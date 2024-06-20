1f582f04305d79ccc4b8285124cc5d0ce73ade61
asano-yukii <asano-yuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 17:39:47 +0900

    Update BoardServlet.java

--- src/servlet/BoardServlet.java
+++ src/servlet/BoardServlet.java
@@ -9,6 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Boards;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 /**
  * Servlet implementation class BoardServlet
  */
@@ -24,16 +32,74 @@ public class BoardServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    // 現在の日付を取得する
-
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/board.jsp");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/board.jsp");
 		dispatcher.forward(request, response);
+    }
+
+
+    // 投稿された日付を格納する 以下は日付が格納されているのでしょうか・・
+    // Boardsのinsertからコピペして書きました
+    public boolean insert(Boards board) {
+		Connection conn = null;
+		boolean result = false;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/B2","sa", "");
+			// SQL文を準備する
+			// この下のところがタイトルと内容と日付で記述すればいいってことでしょうか？ユーザー名前とhouses_id
+			// はセッションスコープに格納されているからいらないってことですか?
+			String sql = "INSERT INTO BOARDS VALUES (?, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+
+			if (board.getTittle() != null && !board.getTittle().equals("")) {
+				pStmt.setString(1, board.getTittle());
+			}
+			else {
+				pStmt.setString(1, "（未設定）");
+			}
+			if (board.getMessage() != null && !board.getMessage().equals("")) {
+				pStmt.setString(2, board.getMessage());
+			}
+			else {
+				pStmt.setString(2, "（未設定）");
+			}
+			if (board.getPost_date() != null && !board.getPost_date().equals("")) {
+				pStmt.setString(3, board.getPost_date());
+			}
+			else {
+				pStmt.setString(3, "（未設定）");
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
 	}
-
-
+	// 結果を返す
+	return result;
+}
 }

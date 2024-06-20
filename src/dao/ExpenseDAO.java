cb36a8ce9ac3d36edd9ac2e17848427287a93a5a
haruna-akiyama <akiyama-haruna-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 18:04:10 +0900

    a

--- src/dao/ExpenseDAO.java
+++ src/dao/ExpenseDAO.java
@@ -3,7 +3,10 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Settlements;
 
@@ -12,7 +15,7 @@ import model.Settlements;
 // DAOの名前は変えるとエラーになるからそのままExpensesDAOのまま
 public class ExpenseDAO {
 
-	// レシート登録
+	// レシート登録、成功したらtrueを返す
 	public boolean insert(Settlements settlement) {
 		Connection conn = null;
 		boolean result = false;
@@ -83,6 +86,71 @@ public class ExpenseDAO {
 	}
 	// 結果を返す
 	return result;
-}
+	}
+
+	// ハウスIDでレシートリストを返す
+	public List <Receipt_Details> list(int houses_id) {
+		Connection conn = null;
+		List<Receipt_Details> ReceiptList = new ArrayList<Receipt_Details>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/B2", "sa", "");
+
+			//SQL文を準備する
+			String sql = "SELECT * FROM RECEIPT_DETAILS WHERE houses_id = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SQL文を完成させる
+			pStmt.setInt(1, houses_id);
+
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Receipt_Details Receipt_record = new Receipt_Details(
+				rs.getInt("ID"),						// ID
+				rs.getInt("users_id"),					//ユーザーID
+				rs.getString("receipt_name"),			//支出の種類
+				rs.getInt("receipt_amount"),			//金額
+				rs.getString("description"),			//詳細
+				rs.getString("expense_date"),			//支払日
+				rs.getString("settlement_finish"),		//精算済みフラグ
+				rs.getString("settlement_approval"),	//承認済みフラグ
+				rs.getString("settlement_date"),		//精算日
+				rs.getInt("houses_id")					//家ID
+				);
+				ReceiptList.add(Receipt_record);
+			}
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			ReceiptList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			ReceiptList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					ReceiptList = null;
+				}
+			}
+		}
+		// 結果を返す
+		return ReceiptList;
+	}
 
 }
